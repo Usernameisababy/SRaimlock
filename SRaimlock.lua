cb0ca99c266a0fdcc97a286996b960cf3b8b2e35
Usernameisababy <133069810+Usernameisababy@users.noreply.github.com>
2025-10-21 22:39:31 -0500

    Update SRaimlock.lua

--- SRaimlock.lua
+++ SRaimlock.lua
@@ -1,2 +1,2 @@
 local oldWait=wait wait=function(...)if...==0 then return 1/30,elapsedTime()end return oldWait(...)end
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Usernameisababy/SRHub/main/actual.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Usernameisababy/SRaimlock/main/actual.lua"))()
